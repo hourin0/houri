d67100b44bb3932dc1d558ad7a464c31d950d8e7
randomCharacterArray <phucjaki@gmail.com>
2022-08-04 13:16:09 +0700

    Testing with ssatori.c

--- ssatori.c
+++ ssatori.c
@@ -13,8 +13,9 @@
 
 U0 ssatori_entry() {
 	putstr_attr("OK\n",GREEN,DEF_BG);
-	putstr("\n\nSSatori Hello World!\n");
+	putstr_attr("\n\nSSatori Hello World!\n",MAGENTA,YELLOW);
 	putstr("Novice user press C-h for a list of built-in commands\n");
+	putstr("Press C-m for a list of defined key shortcuts\n");
 	putstr_attr("@localhost> ",MAGENTA,DEF_BG);
 
 }
